72d49c413fdee6995475d7c4177b77c65ba9c3d6
Young Jun Park <june0313@gmail.com>
2019-08-28 00:09:43 +0900

    htmlStatement 메서드 추가 및 테스트코드 작성

--- src/main/java/refactoring/yj/ch01/example/video_rental_shop/Customer.java
+++ src/main/java/refactoring/yj/ch01/example/video_rental_shop/Customer.java
@@ -36,6 +36,21 @@ public class Customer {
         return result;
     }
 
+    public String htmlStatement() {
+        Enumeration<Rental> rentals = _rentals.elements();
+        String result = "<H1><EM>" + getName() + " 고객님의 대여 기록</EM></h1><P>\n";
+        while (rentals.hasMoreElements()) {
+            Rental each = rentals.nextElement();
+            // 모든 대여 비디오 정보와 대여료를 출력
+            result += each.getMovie().getTitle() + ": " + String.valueOf(each.getCharge()) + "<BR>\n";
+        }
+
+        // 푸터 행 추가
+        result += "<P>누적 대여료: <EM>" + String.valueOf(getTotalCharge()) + "</EM><P>\n";
+        result += "적립 포인트: <EM>" + String.valueOf(getTotalFrequentRenterPoints()) +  "</EM><P>";
+        return result;
+    }
+
     private double getTotalCharge() {
         double result = 0;
         Enumeration<Rental> rentals = _rentals.elements();
--- src/test/java/refactoring/yj/ch01/example/video_rental_shop/CustomerTest.java
+++ src/test/java/refactoring/yj/ch01/example/video_rental_shop/CustomerTest.java
@@ -18,7 +18,7 @@ public class CustomerTest {
     }
 
     @Test
-    public void whenCustomerHasAnyRental() {
+    public void whenCustomerHasAnyRental_statement() {
         // given
         Customer sut = createNewTestCustomer();
 
@@ -32,7 +32,21 @@ public class CustomerTest {
     }
 
     @Test
-    public void rentalOneRegularMovieOneDay() {
+    public void whenCustomerHasAnyRental_htmlStatement() {
+        // given
+        Customer sut = createNewTestCustomer();
+
+        // when
+        String htmlStatement = sut.htmlStatement();
+
+        // then
+        assertThat(htmlStatement).isEqualTo("<H1><EM>jun 고객님의 대여 기록</EM></h1><P>\n"
+                + "<P>누적 대여료: <EM>0.0</EM><P>\n"
+                + "적립 포인트: <EM>0</EM><P>");
+    }
+
+    @Test
+    public void rentalOneRegularMovieOneDay_statement() {
         // given
         Customer sut = createNewTestCustomer();
         sut.addRental(new Rental(aRegularMovie(), 1));
@@ -49,7 +63,24 @@ public class CustomerTest {
     }
 
     @Test
-    public void rentalOneNewReleaseMovieOneDay() {
+    public void rentalOneRegularMovieOneDay_htmlStatement() {
+        // given
+        Customer sut = createNewTestCustomer();
+        sut.addRental(new Rental(aRegularMovie(), 1));
+
+        // when
+        String htmlStatement = sut.htmlStatement();
+
+        // then
+        System.out.println(htmlStatement);
+        assertThat(htmlStatement).isEqualTo("<H1><EM>jun 고객님의 대여 기록</EM></h1><P>\n"
+                + "testMovie: 2.0<BR>\n"
+                + "<P>누적 대여료: <EM>2.0</EM><P>\n"
+                + "적립 포인트: <EM>1</EM><P>");
+    }
+
+    @Test
+    public void rentalOneNewReleaseMovieOneDay_statement() {
         // given
         Customer sut = createNewTestCustomer();
         sut.addRental(new Rental(aNewReleaseMovie(), 1));
@@ -66,7 +97,24 @@ public class CustomerTest {
     }
 
     @Test
-    public void rentalOneChildrenMovieOneDay() {
+    public void rentalOneNewReleaseMovieOneDay_htmlStatement() {
+        // given
+        Customer sut = createNewTestCustomer();
+        sut.addRental(new Rental(aNewReleaseMovie(), 1));
+
+        // when
+        String htmlStatement = sut.htmlStatement();
+
+        // then
+        System.out.println(htmlStatement);
+        assertThat(htmlStatement).isEqualTo("<H1><EM>jun 고객님의 대여 기록</EM></h1><P>\n"
+                + "testMovie: 3.0<BR>\n"
+                + "<P>누적 대여료: <EM>3.0</EM><P>\n"
+                + "적립 포인트: <EM>1</EM><P>");
+    }
+
+    @Test
+    public void rentalOneChildrenMovieOneDay_statement() {
         // given
         Customer sut = createNewTestCustomer();
         sut.addRental(new Rental(aChildrenMovie(), 1));
@@ -83,7 +131,24 @@ public class CustomerTest {
     }
 
     @Test
-    public void rentalOneRegularMovieOverTwoDays() {
+    public void rentalOneChildrenMovieOneDay_htmlStatement() {
+        // given
+        Customer sut = createNewTestCustomer();
+        sut.addRental(new Rental(aChildrenMovie(), 1));
+
+        // when
+        String htmlStatement = sut.htmlStatement();
+
+        // then
+        System.out.println(htmlStatement);
+        assertThat(htmlStatement).isEqualTo("<H1><EM>jun 고객님의 대여 기록</EM></h1><P>\n"
+                + "testMovie: 1.5<BR>\n"
+                + "<P>누적 대여료: <EM>1.5</EM><P>\n"
+                + "적립 포인트: <EM>1</EM><P>");
+    }
+
+    @Test
+    public void rentalOneRegularMovieOverTwoDays_statement() {
         // given
         Customer sut = createNewTestCustomer();
         sut.addRental(new Rental(aRegularMovie(), 3));
@@ -97,6 +162,23 @@ public class CustomerTest {
                 + "\ttestMovie\t3.5\n"
                 + "누적 대여료 : 3.5\n"
                 + "적립 포인트 : 1");
+    }
+
+    @Test
+    public void rentalOneRegularMovieOverTwoDays_htmlStatement() {
+        // given
+        Customer sut = createNewTestCustomer();
+        sut.addRental(new Rental(aRegularMovie(), 3));
+
+        // when
+        String htmlStatement = sut.htmlStatement();
+
+        // then
+        System.out.println(htmlStatement);
+        assertThat(htmlStatement).isEqualTo("<H1><EM>jun 고객님의 대여 기록</EM></h1><P>\n"
+                + "testMovie: 3.5<BR>\n"
+                + "<P>누적 대여료: <EM>3.5</EM><P>\n"
+                + "적립 포인트: <EM>1</EM><P>");
     }
 
     private static Movie aRegularMovie() {
