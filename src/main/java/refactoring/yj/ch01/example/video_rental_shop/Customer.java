5c6b3b83d00b749e291ba0d665dad2b251e27812
Young Jun Park <june0313@gmail.com>
2019-08-27 23:38:25 +0900

    적립 포인트 계산 로직 메서드 추출 및 이동

--- src/main/java/refactoring/yj/ch01/example/video_rental_shop/Customer.java
+++ src/main/java/refactoring/yj/ch01/example/video_rental_shop/Customer.java
@@ -28,12 +28,8 @@ public class Customer {
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
 
-            //적립 포인트 1포인트 증가
-            frequentRenterPoints++;
-
-            //최신물을 이틀 이상 대여하면 보너스 포인트 지급
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
-                frequentRenterPoints++;
+            // 경우에 따른 적립 포인트 지급 함수를 호출
+            frequentRenterPoints += each.getFrequentRenterPoints();
 
             //대여하는 비디오 정보와 대여로를 출력
             result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
--- src/main/java/refactoring/yj/ch01/example/video_rental_shop/Rental.java
+++ src/main/java/refactoring/yj/ch01/example/video_rental_shop/Rental.java
@@ -38,4 +38,13 @@ public class Rental {
 
         return result;
     }
+
+    public int getFrequentRenterPoints() {
+        //최신물을 이틀 이상 대여하면 2 포인트 지급, 그 외엔 1포인트 지급
+        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1) {
+            return 2;
+        } else {
+            return 1;
+        }
+    }
 }
