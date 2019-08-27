5180c1d324f0e2f4e5f33490f12325df46e44230
Young Jun Park <june0313@gmail.com>
2019-08-27 23:28:28 +0900

    amountFor 메서드 이동
    - Customer -> Rental

--- src/main/java/refactoring/yj/ch01/example/video_rental_shop/Customer.java
+++ src/main/java/refactoring/yj/ch01/example/video_rental_shop/Customer.java
@@ -50,24 +50,6 @@ public class Customer {
     }
 
     private double amountFor(Rental rental) {
-        double result = 0;
-
-        switch (rental.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (rental.getDaysRented() > 2)
-                    result += (rental.getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += rental.getDaysRented() * 3;
-                break;
-            case Movie.CHILDREN:
-                result += 1.5;
-                if (rental.getDaysRented() > 3)
-                    result += (rental.getDaysRented() - 3) * 1.5;
-                break;
-        }
-
-        return result;
+        return rental.getCharge();
     }
 }
--- src/main/java/refactoring/yj/ch01/example/video_rental_shop/Rental.java
+++ src/main/java/refactoring/yj/ch01/example/video_rental_shop/Rental.java
@@ -16,4 +16,26 @@ public class Rental {
     public Movie getMovie() {
         return _movie;
     }
+
+    public double getCharge() {
+        double result = 0;
+
+        switch (getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (getDaysRented() > 2)
+                    result += (getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                result += getDaysRented() * 3;
+                break;
+            case Movie.CHILDREN:
+                result += 1.5;
+                if (getDaysRented() > 3)
+                    result += (getDaysRented() - 3) * 1.5;
+                break;
+        }
+
+        return result;
+    }
 }
