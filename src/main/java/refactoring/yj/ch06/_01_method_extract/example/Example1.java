4281f9e730f806e703fe308f63b17d58815bd56c
Young Jun Park <june0313@gmail.com>
2019-09-30 21:55:15 +0900

    [ch6] 메서드 추출 예제1 작성

---
+++ src/main/java/refactoring/yj/ch06/_01_method_extract/example/Example1.java
@@ -0,0 +1,17 @@
+package refactoring.yj.ch06._01_method_extract.example;
+
+public class Example1 {
+    private String name;
+
+    void printOwing(double amount) {
+        printBanner();
+
+        // 세부 정보 출력
+        System.out.println("name:" + name);
+        System.out.println("amount:" + amount);
+    }
+
+    private void printBanner() {
+        System.out.println("=== Banner ===");
+    }
+}
