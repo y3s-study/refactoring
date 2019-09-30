e9e52141d6756f9128e490b04b5cacc87fe85056
조성국 <jsk00123@naver.com>
2019-10-01 00:31:13 +0900

    [ch06] 메서드 추출

--- src/main/java/refactoring/sk/ch06/_01_extract_method/Example2.java
+++ src/main/java/refactoring/sk/ch06/_01_extract_method/Example2.java
@@ -1,4 +1,36 @@
 package refactoring.sk.ch06._01_extract_method;
 
+import java.util.Enumeration;
+import java.util.Vector;
+
 public class Example2 {
+    private String _name = "test";
+    private Vector<Order> _orders;
+
+    void printOwing(double previousAmount) {
+        printBanner();
+        double outstanding = getOutstanding(previousAmount * 1.2);
+        printDetail(outstanding);
+    }
+
+    private void printBanner() {
+        System.out.println("********************");
+        System.out.println("*******고객외상*******");
+        System.out.println("********************");
+    }
+
+    private void printDetail(double amount) {
+        System.out.println("name:" + _name);
+        System.out.println("amount:" + amount);
+    }
+
+    private double getOutstanding(double initialValue) {
+        Enumeration e = _orders.elements();
+        double result = initialValue;
+        while (e.hasMoreElements()) {
+            Order each = (Order) e.nextElement();
+            result += each.getAmount();
+        }
+        return result;
+    }
 }
--- src/main/java/refactoring/sk/ch06/_01_extract_method/Order.java
+++ src/main/java/refactoring/sk/ch06/_01_extract_method/Order.java
@@ -1,4 +1,9 @@
 package refactoring.sk.ch06._01_extract_method;
 
 public class Order {
+    private double Amount;
+
+    public double getAmount() {
+        return Amount;
+    }
 }
