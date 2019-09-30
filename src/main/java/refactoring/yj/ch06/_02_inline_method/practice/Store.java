4b31a6ab1cc5a8b0ab932a46b35b3fb19e37e5c0
Young Jun Park <june0313@gmail.com>
2019-09-30 22:15:40 +0900

    [ch6] 인라인 메서드 예제 작성

---
+++ src/main/java/refactoring/yj/ch06/_02_inline_method/practice/Store.java
@@ -0,0 +1,13 @@
+package refactoring.yj.ch06._02_inline_method.practice;
+
+public class Store {
+    private int numberOfLateDeliveries = 0;
+
+    int getRating() {
+        return (moreThanFiveLateDeliveries()) ? 2 : 1;
+    }
+
+    private boolean moreThanFiveLateDeliveries() {
+        return numberOfLateDeliveries > 5;
+    }
+}
