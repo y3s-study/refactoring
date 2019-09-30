d414f67d0213b8484aa5b5f010e2199294622f77
Young Jun Park <june0313@gmail.com>
2019-09-30 23:33:03 +0900

    [ch6] 직관적 임시 변수 사용 예제 작성

---
+++ src/main/java/refactoring/yj/ch06/_05_explaining_var/practice/Order1.java
@@ -0,0 +1,13 @@
+package refactoring.yj.ch06._05_explaining_var.practice;
+
+public class Order1 {
+    private int quantity;
+    private int itemPrice;
+
+    public double price() {
+        // 결제액(price) = 총 구매액(base price) - 대량 구매 할인(quantity discount) + 배송비(shipping)
+        return quantity * itemPrice -
+                Math.max(0, quantity - 500) * itemPrice * 0.05 +
+                Math.min(quantity * itemPrice * 0.1, 100.0);
+    }
+}
