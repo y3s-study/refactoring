9ea178669c2e67a73866c83279c23dd07fa0c0de
Young Jun Park <june0313@gmail.com>
2019-10-01 23:34:15 +0900

    [ch6] 알고리즘 리팩토링

--- src/main/java/refactoring/yj/ch06/_09_subtitute_algorithm/practice/PersonFinder.java
+++ src/main/java/refactoring/yj/ch06/_09_subtitute_algorithm/practice/PersonFinder.java
@@ -1,16 +1,15 @@
 package refactoring.yj.ch06._09_subtitute_algorithm.practice;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class PersonFinder {
     public String foundPerson(String[] people) {
-        for (int i = 0; i < people.length; i++) {
-            if (people[i].equals("Don")) {
-                return "Don";
-            }
-            if (people[i].equals("John")) {
-                return "John";
-            }
-            if (people[i].equals("Kent")) {
-                return "Kent";
+        List<String> candidates = Arrays.asList("Don", "John", "Kent");
+
+        for (String person : people) {
+            if (candidates.contains(person)) {
+                return person;
             }
         }
 
