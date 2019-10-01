2d0497661ffc4c402cd4f371257a81209b074362
Young Jun Park <june0313@gmail.com>
2019-10-01 23:07:49 +0900

    [ch6] 매개변수로의 값 대입 제거 예제 작성

---
+++ src/main/java/refactoring/yj/ch06/_07_remove_assign_to_param/practice/Discounter.java
@@ -0,0 +1,10 @@
+package refactoring.yj.ch06._07_remove_assign_to_param.practice;
+
+public class Discounter {
+    public int discount(int inputVal, int quantity, int yearToDate) {
+        if (inputVal > 50) inputVal -= 2;
+        if (quantity > 100) inputVal -= 1;
+        if (yearToDate > 10_000) inputVal -= 4;
+        return inputVal;
+    }
+}
