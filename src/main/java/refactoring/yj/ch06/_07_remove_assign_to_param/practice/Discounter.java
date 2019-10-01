831f739c7daa2620b878466073b1392fe5ea0f22
Young Jun Park <june0313@gmail.com>
2019-10-01 23:11:06 +0900

    [ch6] 매개변수를 임시변수 사용으로 변경

--- src/main/java/refactoring/yj/ch06/_07_remove_assign_to_param/practice/Discounter.java
+++ src/main/java/refactoring/yj/ch06/_07_remove_assign_to_param/practice/Discounter.java
@@ -1,10 +1,11 @@
 package refactoring.yj.ch06._07_remove_assign_to_param.practice;
 
 public class Discounter {
-    public int discount(int inputVal, int quantity, int yearToDate) {
-        if (inputVal > 50) inputVal -= 2;
-        if (quantity > 100) inputVal -= 1;
-        if (yearToDate > 10_000) inputVal -= 4;
-        return inputVal;
+    public int discount(final int inputVal, final int quantity, final int yearToDate) {
+        int result = inputVal;
+        if (inputVal > 50) result -= 2;
+        if (quantity > 100) result -= 1;
+        if (yearToDate > 10_000) result -= 4;
+        return result;
     }
 }
