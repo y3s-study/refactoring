b3e1c4260c81c7262d8db21853d3ef19d287e113
Young Jun Park <june0313@gmail.com>
2019-10-01 22:38:50 +0900

    [ch6] 임시 변수 분리 예제 작성

---
+++ src/main/java/refactoring/yj/ch06/_06_split_temp_var/example/Haggis.java
@@ -0,0 +1,22 @@
+package refactoring.yj.ch06._06_split_temp_var.example;
+
+public class Haggis {
+    private double primaryForce;
+    private double secondaryForce;
+    private double mass;
+    private int delay;
+
+    public double getDistanceTravelled(int time) {
+        double result;
+        double acc = primaryForce * mass;
+        int primaryTime = Math.min(time, delay);
+        result = 0.5 * acc * primaryTime * primaryTime;
+        int secondaryTime = time - delay;
+        if (secondaryTime > 0) {
+            double primaryVel = acc * delay;
+            acc = (primaryForce + secondaryForce) / mass;
+            result += primaryVel * secondaryTime + 0.5 * acc * secondaryTime * secondaryTime;
+        }
+        return result;
+    }
+}
