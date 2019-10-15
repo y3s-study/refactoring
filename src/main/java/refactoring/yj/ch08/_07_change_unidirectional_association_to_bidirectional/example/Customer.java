cee6a4389ac9fb4d36a45b550bf2fc615bd01869
Young Jun Park <june0313@gmail.com>
2019-10-15 21:49:15 +0900

    [ch8] 8장 예제 작성

---
+++ src/main/java/refactoring/yj/ch08/_07_change_unidirectional_association_to_bidirectional/example/Customer.java
@@ -0,0 +1,4 @@
+package refactoring.yj.ch08._07_change_unidirectional_association_to_bidirectional.example;
+
+public class Customer {
+}
---
+++ src/main/java/refactoring/yj/ch08/_16_replace_subclass_with_fields/example/Male.java
@@ -0,0 +1,13 @@
+package refactoring.yj.ch08._16_replace_subclass_with_fields.example;
+
+class Male extends Person {
+    @Override
+    boolean isMale() {
+        return true;
+    }
+
+    @Override
+    char getCode() {
+        return 'M';
+    }
+}
