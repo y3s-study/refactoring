243b820f13c78197f128e2d8803abf8f67f1c668
Young Jun Park <june0313@gmail.com>
2019-10-22 21:06:08 +0900

    [ch9] 조건문 간결화 예제 작성

---
+++ src/main/java/refactoring/yj/ch09/_06_replace_conditional_with_polymorphism/example/Salesman.java
@@ -0,0 +1,8 @@
+package refactoring.yj.ch09._06_replace_conditional_with_polymorphism.example;
+
+public class Salesman extends EmployeeType {
+    @Override
+    int getTypeCode() {
+        return EmployeeType.SALESMAN;
+    }
+}
