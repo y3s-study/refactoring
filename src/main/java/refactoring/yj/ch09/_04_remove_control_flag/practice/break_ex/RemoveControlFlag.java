85a292c089aa62c05dc9e387fd778a7eacab851d
Young Jun Park <june0313@gmail.com>
2019-10-22 21:27:28 +0900

    [ch9] 제어 플래그 제거

---
+++ src/main/java/refactoring/yj/ch09/_04_remove_control_flag/practice/break_ex/RemoveControlFlag.java
@@ -0,0 +1,26 @@
+package refactoring.yj.ch09._04_remove_control_flag.practice.break_ex;
+
+/**
+ * Remove Control Flag (제어 플래그 제거)
+ * <p>
+ * 논리 연산식의 제어 플래그 역할을 하는 변수가 있을 땐
+ * - 그 변수를 break문이나 return문으로 바꾸자
+ */
+public class RemoveControlFlag {
+    void checkSecurity(String[] people) {
+        for (String aPeople : people) {
+            if (aPeople.equals("Don")) {
+                sendAlert();
+                break;
+            }
+            if (aPeople.equals("John")) {
+                sendAlert();
+                break;
+            }
+        }
+    }
+
+    private void sendAlert() {
+        System.out.println("alert");
+    }
+}
